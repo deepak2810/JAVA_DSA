783a1b3045f0abc0c76f3750d786c0e9adf9fc03
deepak2810 <deepakkhanka.97@gmail.com>
2022-09-12 12:59:57 +0530

    ArrayList in jva

---
+++ src/2d_array/ArrayListExmp.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ArrayListExmp {
+    public static void main(String[] args) {
+//
+//        Syntax for ArrayList
+
+        ArrayList<Integer> list = new ArrayList<>(10);
+
+        // To add an element to the list
+        list.add(65);
+        list.add(645);
+        list.add(651);
+        list.add(653);
+        list.add(6125);
+
+        System.out.println(list);
+
+    }
+}
